5989616e0fbe8d442515e19e45f3dfec936cd2f0
Aritra212 <aritrapaul20622@gmail.com>
2025-02-11 16:04:30 +0530

    interface added

---
+++ src/app/common/common.interface.ts
@@ -0,0 +1,37 @@
+export interface LogoutProps {
+  noredirect: boolean;
+}
+
+export interface IUser {
+  created_at: string;
+  email: string;
+  name: string;
+  user_id: string;
+  user_name: string;
+  avatar_url?: string;
+}
+
+export interface IUserShort {
+  name: string;
+  user_id: string;
+  user_name: string;
+  avatar_url?: string;
+}
+
+export interface IConversationData {
+  id: string;
+  created_at: string;
+  user: IUser;
+  last_message: string | null;
+  notifications?: number;
+}
+
+export interface IChatData {
+  chat_id: string;
+  created_at: string;
+  status?: "Pending" | "Sent" | "Seen";
+  conversation_id: string;
+  message: string;
+  is_edited: boolean;
+  sender_id: IUser;
+}
